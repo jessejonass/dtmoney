9a4180156d1038f9843f0bad224ef2664ef44444
Jesse Jonas <jessejonas13@gmail.com>
2021-03-16 19:40:33 -0300

    feat: adding miragejs and axios

---
+++ README.md
@@ -0,0 +1,15 @@
+Clone repository
+
+> git clone https://github.com/jessejonass/dtmoney.git
+
+Install the dependencies
+
+> yarn install
+
+Execute the fake api
+
+> command here
+
+Execute the application
+
+> yarn start
--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,16 @@
+import { useEffect } from 'react';
+import { api } from '../../services/api';
+
 import { Container } from './styles';
 
 export function TransactionsTable() {
+  // const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    api.get('transactions')
+    .then(data => console.log(data));
+  }, []);
+
   return (
     <Container>
       <table>
